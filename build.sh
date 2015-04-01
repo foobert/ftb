ad4d4d635f95c587cf5fb67679a7c4b4c50a2826
foobert <0x002a@gmail.com>
2015-04-01 19:02:31 +0200

    Update to FTB Infinity 1.3.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-[ -f modpacks^FTBInfinity^1_2_1^FTBInfinityServer.zip ] || wget http://www.creeperrepo.net/FTB2/modpacks%5EFTBInfinity%5E1_2_1%5EFTBInfinityServer.zip
+[ -f modpacks^FTBInfinity^1_3_1^FTBInfinityServer.zip ] || wget http://www.creeperrepo.net/FTB2/modpacks%5EFTBInfinity%5E1_3_1%5EFTBInfinityServer.zip
 if [ $(uname -s) = "Darwin" -a "x$DOCKER_HOST" = "x" ]; then
     echo Configuring boot2docker
     $(boot2docker shellinit)
